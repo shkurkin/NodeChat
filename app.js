714fd14e04c3edf93c715d4a278da929ef19dd6c
Eli Shkurkin <eshkurkin@gmail.com>
2014-03-24 18:31:30 -0700

    Add socket.io
    Basic chat functionality running

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var port = 3700;
 var path = require('path');
+var http = require('http');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -12,9 +13,8 @@ var routes = require("./routes");
 
 app.get("/", routes.index);
 
-app.listen(port);
-
-var io = require('socket.io').listen(app.listen(port));
+var server = http.createServer(app)
+var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket) {
   socket.emit('message', { message: 'Welcome to Just Chat' });
@@ -22,4 +22,6 @@ io.sockets.on('connection', function(socket) {
     io.sockets.emit('message', data);
   })
 })
+
+server.listen(port)
 console.log("Listening on port " + port);
